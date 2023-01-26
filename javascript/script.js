e2dfb4f4dde223278730d97d038c15e9e9fb0ac3
benjamin-crew <benncrew94@gmail.com>
2023-01-26 17:16:31 +0000

    Add grid and hover event listeners

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,57 @@
 // Generate grid
 const gridSize = 16;
-var gridContainer = document.querySelector('.main-container');
+const gridContainer = document.querySelector('.main-container');
+
+// Set colours
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'indigo',
+    'violet'
+];
+let colorSelector = 0;
 
 // Rows
 for (let i = 0; i < gridSize; i++) {
-    console.log(`Creating gridRow-${i}`)
-    let gridRow = document.createElement('div');
-    gridRow.classList.add(`gridRow-${i}`);
-    gridRow.style.cssText = 'border-style: solid; border-color: red;';
+    const gridRow = document.createElement('div');
+    gridRow.classList.add('gridRow', `gridRow-${i}`);
+    // gridRow.style.cssText = 'border-style: solid; border-color: red;';
 
     // Columns    
     for (let j = 0; j < gridSize; j++) {
-        console.log(`Creating gridDiv-${i} index ${j}`)
-        let gridRowIndex = document.createElement('div');
-        gridRowIndex.classList.add(`gridRowIndex-${j}`);
-        gridRowIndex.style.cssText = 'border-style: solid; border-color: green;'; 
-        gridContainer = gridContainer.appendChild(gridRowIndex);
-        console.log(`Created gridDiv-${i} index ${j}`)
+        const gridColumnIndex = document.createElement('div');
+        gridColumnIndex.classList.add('grid-item', 'gridColumn', `gridColumnIndex-${j}`);
+        gridColumnIndex.style.cssText = 'border-style: solid;';
+        gridRow.appendChild(gridColumnIndex);
     }
     // Append to main container
     gridContainer.appendChild(gridRow);
 }
+
+const gridItems = document.querySelectorAll('.grid-item')
+
+gridItems.forEach(function (item) {
+    item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = colors[colorSelector];
+        if (colorSelector < colors.length) {
+            colorSelector++;
+        } else {
+            colorSelector = 0;
+        }
+    });
+    // item.addEventListener('mouseout', () => {
+    //     console.log(item.className);
+    //     item.style.backgroundColor = 'white';
+    // });
+});
+
+// Clear screen
+const clearBtn = document.getElementById("clear-button");
+clearBtn.addEventListener('click', () => {
+    gridItems.forEach(function (item) {
+        item.style.backgroundColor = 'white';
+    });
+});
