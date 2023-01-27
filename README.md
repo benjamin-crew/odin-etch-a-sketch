ee79acc5c95ce213170a711f9f3d4717e1300088
benjamin-crew <benncrew94@gmail.com>
2023-01-27 09:07:12 +0000

    Add grid size slider. Project complete for now

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # odin-etch-a-sketch
 The Odin Project - Etch-a-sketch/Scratchpad website, created with JS, HTML, and CSS
+
+## The Odin Project
+Simple implementation of an Etch-A-Sketch in JavaScript
+
+## Live Preview
+https://benjamin-crew.github.io/odin-etch-a-sketch/
+
+## Future updates
+- Fixed size of grid on screen, regardless of the grid size
+- Add functionality for rainbow button and color picker
+- Make the website look nicer. This was a Javascript learning exercise so not much attention has been paid to the overall aesthetic. 
--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,55 @@
+function generateGrid(size){
+
+    // Start fresh
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    // Rows
+    for (let i = 0; i < size; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('gridRow', `gridRow-${i}`);
+        // gridRow.style.cssText = 'border-style: solid; border-color: red;';
+
+        // Columns    
+        for (let j = 0; j < size; j++) {
+            const gridColumnIndex = document.createElement('div');
+            gridColumnIndex.classList.add('grid-item', 'gridColumn', `gridColumnIndex-${j}`);
+            gridColumnIndex.style.cssText = 'border-style: solid;';
+            gridRow.appendChild(gridColumnIndex);
+        }
+        // Append to main container
+        gridContainer.appendChild(gridRow);
+
+        // Output grid size
+        const gridSizeLabel = document.querySelector("#gridSizeValue");
+        gridSizeLabel.textContent = size;
+    }
+
+    // Add the event listeners
+    const gridItems = document.querySelectorAll('.grid-item')
+
+    gridItems.forEach(function (item) {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = colors[colorSelector];
+            if (colorSelector < colors.length) {
+                colorSelector++;
+            } else {
+                colorSelector = 0;
+            }
+        });
+        // item.addEventListener('mouseout', () => {
+        //     console.log(item.className);
+        //     item.style.backgroundColor = 'white';
+        // });
+    });
+}
+
 // Generate grid
-const gridSize = 16;
-const gridContainer = document.querySelector('.main-container');
+let gridSize = 16;
+const gridContainer = document.querySelector('.grid-container');
+
+generateGrid(gridSize)
 
 // Set colours
 const colors = [
@@ -14,44 +63,18 @@ const colors = [
 ];
 let colorSelector = 0;
 
-// Rows
-for (let i = 0; i < gridSize; i++) {
-    const gridRow = document.createElement('div');
-    gridRow.classList.add('gridRow', `gridRow-${i}`);
-    // gridRow.style.cssText = 'border-style: solid; border-color: red;';
-
-    // Columns    
-    for (let j = 0; j < gridSize; j++) {
-        const gridColumnIndex = document.createElement('div');
-        gridColumnIndex.classList.add('grid-item', 'gridColumn', `gridColumnIndex-${j}`);
-        gridColumnIndex.style.cssText = 'border-style: solid;';
-        gridRow.appendChild(gridColumnIndex);
-    }
-    // Append to main container
-    gridContainer.appendChild(gridRow);
-}
-
-const gridItems = document.querySelectorAll('.grid-item')
-
-gridItems.forEach(function (item) {
-    item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = colors[colorSelector];
-        if (colorSelector < colors.length) {
-            colorSelector++;
-        } else {
-            colorSelector = 0;
-        }
-    });
-    // item.addEventListener('mouseout', () => {
-    //     console.log(item.className);
-    //     item.style.backgroundColor = 'white';
-    // });
-});
-
 // Clear screen
 const clearBtn = document.getElementById("clear-button");
 clearBtn.addEventListener('click', () => {
+    const gridItems = document.querySelectorAll('.grid-item')
     gridItems.forEach(function (item) {
         item.style.backgroundColor = 'white';
     });
+});
+
+// Change grid size
+let gridSizeInput = document.querySelector("#gridSizeRange");
+gridSizeInput.addEventListener('input', () => {
+    const gridSizeValue = document.getElementById("gridSizeRange").value;
+    generateGrid(gridSizeValue);
 });
